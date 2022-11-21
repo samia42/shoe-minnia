9f63f6ccb59f5dc88c9c973d19e88a188cf5e69f
samia42 <samiasaif42@gmail.com>
2022-11-21 18:48:49 +0500

    deleted errors

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const Connection = async () => {
   const URL = process.env.DB_URI;
-  console.log(URL, "url");
+  console.log(URL);
   try {
     await mongoose.connect(`mongodb+srv://samia:1234@cluster0.oysuehh.mongodb.net/test?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
